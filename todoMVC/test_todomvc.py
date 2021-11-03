22d6af466c31cba14ee15ab672c11e72e058d3ff
Pulinech <pulinech1@gmail.com>
2021-11-03 17:16:08 +0200

    Code refactoring for better css locators and readabylity

---
+++ todoMVC/test_todomvc.py
@@ -0,0 +1,33 @@
+import time
+
+from selene.support.shared.jquery_style import s, ss
+from selene.support.shared import browser
+from selene import have
+
+browser.open_url("https://todomvc.com/examples/react/")
+tasks_list = ["a", "1"]
+
+
+def test_common_tasks_management():
+    # Add
+    s(".new-todo").type('a').press_enter()
+    s(".new-todo").type('b').press_enter()
+    s(".new-todo").type('c').press_enter()
+    ss(".todo-list>li").should(have.exact_texts('a', 'b', 'c'))
+
+    # Edit
+    ss(".todo-list>li").element_by(have.exact_text('b')).double_click()
+    ss(".todo-list>li").element_by(have.css_class("editing")).s(".edit").type(' edited').press_enter()
+
+    # Complete & Clear
+    ss(".todo-list>li").element_by(have.exact_text('b edited')).s(".toggle").click()
+    s(".clear-completed").click()
+    ss(".todo-list>li").should(have.no.exact_text('b edited'))
+
+    # Cancel Edit
+    ss(".todo-list>li").element_by(have.exact_text('c')).double_click()
+    ss(".todo-list>li").element_by(have.css_class("editing")).s(".edit").type(" to be canceled").press_escape()
+
+    # Delete
+    ss(".todo-list>li").element_by(have.exact_text('c')).hover().s(".destroy").click()
+    ss(".todo-list>li").should(have.exact_text('a'))
